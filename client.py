4d8f5868ca8599740991c74daa10e4d8c5909cbc
ZafarFaraz2020 <69028699+ZafarFaraz2020@users.noreply.github.com>
2020-08-05 11:29:29 +1000

    Add files via upload

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 import socket
 
+headersize = 10
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('0.0.0.0', 8080))
-stringss = "This is your client speaking<br>"
-byt = stringss.encode()
-client.send(byt)
-from_server = client.recv(4096)
-client.close()
-print(from_server)
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = client.recv(16)
+        if new_msg:
+            print(f"new message length : {msg[:headersize]}")
+            msglen = int(msg[:headersize])
+            new_msg = False
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - headersize == msglen:
+            print("full message recieved")
+            print(full_msg[headersize:])
+            new_msg = True
+            full_msg = ''
+
+print(full_msg)
--- server.py
+++ server.py
@@ -1,19 +1,23 @@
 import socket
+import time
+
+headersize = 10
+
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
+
 while True:
     conn, addr = serv.accept()
-    from_client = ''
+    print(f"Connection from {addr} has been established!")
+    msg = 'This is your server speaking'
+    msg = f'{len(msg):<{headersize}}' + msg
+    conn.send(bytes(msg, "utf-8"))
     while True:
-        data_encode = conn.recv(4096)
-        data = data_encode.decode()
-        if not data: break
-        from_client += data
-        print(from_client)
-        stringss = "This is your server speaking"
-        sen = stringss.encode()
-        conn.send(sen)
-    conn.close()
+        time.sleep(3)
+        msg = f"The time is : {time.time()}"
+        msg = f'{len(msg):<{headersize}}' + msg
+        conn.send(bytes(msg, "utf-8"))
+
     print('Client is no more')
